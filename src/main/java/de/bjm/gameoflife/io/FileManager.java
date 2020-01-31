8ca82ee5e146cc2b9902a7acdf56fb0372f4ff00
b.jm021 <benmeyer@outlook.de>
2020-01-31 21:57:56 +0100

    Add File Logic

--- src/main/java/de/bjm/gameoflife/io/FileManager.java
+++ src/main/java/de/bjm/gameoflife/io/FileManager.java
@@ -1,16 +1,18 @@
 package de.bjm.gameoflife.io;
 
 import de.bjm.gameoflife.Launcher;
+import javafx.application.Platform;
 
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class FileManager {
 
-    public static boolean saveFile(File saveFile) {
+    public boolean saveFile(File saveFile) {
 
         StringBuilder sb = new StringBuilder();
 
@@ -40,26 +42,34 @@ public class FileManager {
 
     }
 
-    public boolean readFile(File file) {
-        try {
-            List<String> lines =  Files.readAllLines(file.toPath());
+    public void readFile(File file) {
+        //Launcher.getInstance().stopGame();
+        Launcher.getInstance().getOptionsUI().getController().stopGame();
+        Launcher.getInstance().getOptionsUI().getController().resetField();
 
-            String read = lines.get(0);
-            String[] fields = read.split(",");
 
-            for (String field:fields) {
-                int i = Integer.parseInt(field.split(":")[0]);
-                int j = Integer.parseInt(field.split(":")[1]);
-                Launcher.getInstance().getGame().getFeld()[i][j] = 1;
+        Platform.runLater(() -> {
+            try {
+                Launcher.getInstance().getGame().clear();
+                List<String> lines =  Files.readAllLines(file.toPath());
+
+                String read = lines.get(0);
+                String[] fields = read.split(",");
+
+                for (String field:fields) {
+                    int i = Integer.parseInt(field.split(":")[0]);
+                    int j = Integer.parseInt(field.split(":")[1]);
+                    Launcher.getInstance().getGame().getFeld()[i][j] = 1;
+                    System.out.println("Adding field:" + i + " " + j);
+                }
+
+
+                Launcher.getInstance().getRunningMainUI().getRunningController().drawField(Launcher.getInstance().getGame().getFeld());
+
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-
-            return true;
-
-
-        } catch (IOException e) {
-            e.printStackTrace();
-            return false;
-        }
+        });
     }
 
 }
